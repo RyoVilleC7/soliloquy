cc523ac1d47a4d9329794bfaae0f691d60bf4015
RyoVilleC7 <65658462+RyoVilleC7@users.noreply.github.com>
2020-12-28 17:19:53 +0900

    GA追加

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,8 +4,12 @@ import '../styles/foundations/reset.scss'
 import Layout from '../components/basic/layout'
 import { Provider } from 'react-redux'
 import { useStore } from '../store/store'
+import Router from 'next/router'
+import * as gtag from '../lib/gtag'
 
 function MyApp({ Component, pageProps }) {
+
+  Router.events.on('routeChangeComplete', url => gtag.pageview(url))
 
   //create store
   const store = useStore(pageProps.initialReduxState)
