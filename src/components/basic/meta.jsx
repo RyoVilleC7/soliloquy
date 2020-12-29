4d1a944d5de21076ee6a4b35ad79b05394549fdc
RyoVilleC7 <65658462+RyoVilleC7@users.noreply.github.com>
2020-12-29 20:08:44 +0900

    ツイッターカード追加

--- src/components/basic/meta.jsx
+++ src/components/basic/meta.jsx
@@ -10,9 +10,11 @@ const Meta = () => {
             <meta property="og:title" content="Soliloquy" />
             <meta property="og:type" content="website" />
             <meta property="og:url" content="ページの URL" />
-            <meta property="og:image" content="サムネイル画像の URL" />
+            <meta property="og:image" content="https://soliloquy.vercel.app/images/default-image.jpg" />
             <meta property="og:site_name" content="サイト名" />
             <meta property="og:description" content="ページのディスクリプション" />
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta name="twitter:site" content="@ryotaro_WAVES" />
         </Head>
     )
 }
