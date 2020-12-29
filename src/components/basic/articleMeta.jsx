23a30f2c16edc79ec7a52629b731717f03c839fd
RyoVilleC7 <65658462+RyoVilleC7@users.noreply.github.com>
2020-12-29 20:25:05 +0900

    修正

--- src/components/basic/articleMeta.jsx
+++ src/components/basic/articleMeta.jsx
@@ -8,13 +8,14 @@ const ArticleMeta = (props) => {
             <title>{props.title ? `${props.title} | Soliloquy` : "Soliloquy"}</title>
             <meta name="description" content={props.excerpt ? props.excerpt : "This site name is Soliloquy."} />
             <meta property="og:title" content={props.title ? props.title : "Soliloquy"} />
-            <meta property="og:type" content={props.title ? "article" : "website"} />
+            <meta property="og:type" content="website" />
             <meta property="og:url" content={`https://soliloquy.vercel.app/archive/posts/${props.slug}`} />
             <meta property="og:image" content={props.thumbnail} />
             <meta property="og:site_name" content={props.title} />
             <meta property="og:description" content={props.excerpt} />
             <meta name="twitter:card" content="summary_large_image" />
             <meta name="twitter:site" content="@ryotaro_WAVES" />
+            <meta name="twitter:image" content={props.thumbnail} />
         </Head>
     )
 }
